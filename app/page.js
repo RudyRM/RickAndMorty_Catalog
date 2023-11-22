b8cd68695333ca47e8ad9ef789bda1dc6322794e
ender <ender@pop-os.localdomain>
2023-11-22 12:31:01 -0300

    Eliminación de errores

--- app/page.js
+++ app/page.js
@@ -50,7 +50,9 @@ import {
 
 export default function Main() {
   // useState cambia los valores de las variables en tiempo de ejecución
-  const [pagina, setPagina] = useState("https://rickandmortyapi.com/api/character");
+  const [pagina, setPagina] = useState(
+    "https://rickandmortyapi.com/api/character"
+  );
   const [datos, setDatos] = useState({});
   const [loading, setLoading] = useState(true);
 
@@ -70,66 +72,120 @@ export default function Main() {
 
     setLoading(true);
 
-    if(boton.id.includes('boton-primera-pag'))
-      setPagina("https://rickandmortyapi.com/api/character?page=1")
-    else if(boton.id.includes('boton-ultima-pag'))
-      setPagina("https://rickandmortyapi.com/api/character?page=42")
-    else if(boton.id.includes('boton-siguiente-pag'))
+    if (boton.id.includes("boton-primera-pag"))
+      setPagina("https://rickandmortyapi.com/api/character?page=1");
+    else if (boton.id.includes("boton-ultima-pag"))
+      setPagina("https://rickandmortyapi.com/api/character?page=42");
+    else if (boton.id.includes("boton-siguiente-pag"))
       setPagina(datos.info.next);
-    else if(boton.id.includes('boton-anterior-pag'))
+    else if (boton.id.includes("boton-anterior-pag"))
       setPagina(datos.info.prev);
-
-  }
+  };
 
   return (
     <div className="center">
       {loading ? (
         <div className="pantalla-carga">
-          <CircularProgress isIndeterminate
+          <CircularProgress
+            isIndeterminate
             color="teal.700"
-            thickness='15px'
-            size='15vh'/>
+            thickness="15px"
+            size="15vh"
+          />
         </div>
       ) : (
         <div>
           <div className="logo">
-            <img src={"/rm_logo.png"}/>
+            <img src={"/rm_logo.png"} />
           </div>
           <div className="boton-filtro">
-            <ModalFiltros/>
+            <ModalFiltros />
           </div>
           <div className="contenedor-botones">
             <ButtonGroup spacing="2vw">
-              <Button className = "boton-avance" onClick={CambioPagina} id = "boton-primera-pag" leftIcon={<ArrowLeftIcon />}></Button>
-              <Button className = "boton-avance" onClick={CambioPagina} id = "boton-anterior-pag">Página Anterior</Button>
-              <Button className = "boton-avance" onClick={CambioPagina} id = "boton-siguiente-pag">Página siguiente</Button>
-              <Button className = "boton-avance" onClick={CambioPagina} id = "boton-ultima-pag" rightIcon={<ArrowRightIcon />}></Button>
+              <Button
+                className="boton-avance"
+                onClick={CambioPagina}
+                id="boton-primera-pag"
+                leftIcon={<ArrowLeftIcon />}
+              ></Button>
+              <Button
+                className="boton-avance"
+                onClick={CambioPagina}
+                id="boton-anterior-pag"
+              >
+                Página Anterior
+              </Button>
+              <Button
+                className="boton-avance"
+                onClick={CambioPagina}
+                id="boton-siguiente-pag"
+              >
+                Página siguiente
+              </Button>
+              <Button
+                className="boton-avance"
+                onClick={CambioPagina}
+                id="boton-ultima-pag"
+                rightIcon={<ArrowRightIcon />}
+              ></Button>
             </ButtonGroup>
           </div>
-          <SimpleGrid columns={{ base: 2, lg: 4}} className="grilla-cartas">
-            {(datos.results).map((info) => (
-              <Mostrar key={info.name} info={info} />
+          <SimpleGrid columns={{ base: 2, lg: 4 }} className="grilla-cartas">
+            {datos.results.map((info, index) => (
+              <Mostrar key={index} info={info} />
             ))}
           </SimpleGrid>
           <div>
             <ButtonGroup spacing="2vw" className="contenedor-botones">
-              <Button className = "boton-avance" onClick={CambioPagina} id = "boton-primera-pag2" leftIcon={<ArrowLeftIcon />}></Button>
-              <Button className = "boton-avance" onClick={CambioPagina} id = "boton-anterior-pag2">Página Anterior</Button>
-              <Button className = "boton-avance" onClick={CambioPagina} id = "boton-siguiente-pag2">Página siguiente</Button>
-              <Button className = "boton-avance" onClick={CambioPagina} id = "boton-ultima-pag2" rightIcon={<ArrowRightIcon />}></Button>
+              <Button
+                className="boton-avance"
+                onClick={CambioPagina}
+                id="boton-primera-pag2"
+                leftIcon={<ArrowLeftIcon />}
+              ></Button>
+              <Button
+                className="boton-avance"
+                onClick={CambioPagina}
+                id="boton-anterior-pag2"
+              >
+                Página Anterior
+              </Button>
+              <Button
+                className="boton-avance"
+                onClick={CambioPagina}
+                id="boton-siguiente-pag2"
+              >
+                Página siguiente
+              </Button>
+              <Button
+                className="boton-avance"
+                onClick={CambioPagina}
+                id="boton-ultima-pag2"
+                rightIcon={<ArrowRightIcon />}
+              ></Button>
             </ButtonGroup>
           </div>
-          <Text className="texto-company">Copyright © 2023 MERF Company. Chile.</Text>
+          <Text className="texto-company">
+            Copyright © 2023 MERF Company. Chile.
+          </Text>
           <Script>
-              if ({datos.info.prev == null}) document.getElementById('boton-primera-pag').style.display = 'none';
-              if ({datos.info.prev == null}) document.getElementById('boton-anterior-pag').style.display = 'none';
-              if ({datos.info.next == null}) document.getElementById('boton-siguiente-pag').style.display = 'none';
-              if ({datos.info.next == null}) document.getElementById('boton-ultima-pag').style.display = 'none';
-
-              if ({datos.info.prev == null}) document.getElementById('boton-primera-pag2').style.display = 'none';
-              if ({datos.info.prev == null}) document.getElementById('boton-anterior-pag2').style.display = 'none';
-              if ({datos.info.next == null}) document.getElementById('boton-siguiente-pag2').style.display = 'none';
-              if ({datos.info.next == null}) document.getElementById('boton-ultima-pag2').style.display = 'none';
+            if ({datos.info.prev == null})
+            document.getElementById('boton-primera-pag').style.display = 'none';
+            if ({datos.info.prev == null})
+            document.getElementById('boton-anterior-pag').style.display =
+            'none'; if ({datos.info.next == null})
+            document.getElementById('boton-siguiente-pag').style.display =
+            'none'; if ({datos.info.next == null})
+            document.getElementById('boton-ultima-pag').style.display = 'none';
+            if ({datos.info.prev == null})
+            document.getElementById('boton-primera-pag2').style.display =
+            'none'; if ({datos.info.prev == null})
+            document.getElementById('boton-anterior-pag2').style.display =
+            'none'; if ({datos.info.next == null})
+            document.getElementById('boton-siguiente-pag2').style.display =
+            'none'; if ({datos.info.next == null})
+            document.getElementById('boton-ultima-pag2').style.display = 'none';
           </Script>
         </div>
       )}
@@ -139,14 +195,19 @@ export default function Main() {
 
 function Mostrar({ info }) {
   return (
-    <Card className="carta" mb={2} size= "sm">
+    <Card className="carta" mb={2} size="sm">
       <CardHeader>{info.name}</CardHeader>
       <CardBody>
         <Image
+          alt="foto-carta"
           className="imagen-personaje"
-          href={info.name == "Rick Sanchez" ? "" : "https://www.youtube.com/watch?v=x4LqqxYQhtQ"}
+          href={
+            info.name == "Rick Sanchez"
+              ? ""
+              : "https://www.youtube.com/watch?v=x4LqqxYQhtQ"
+          }
           priority
-          style={{ width: '100%', height: 'auto', borderRadius: '4%'}}
+          style={{ width: "100%", height: "auto", borderRadius: "4%" }}
           src={info.image}
           width={"100"}
           height={"100"}
@@ -170,22 +231,22 @@ function InformacionDrawer({ info }) {
   var mes = fechaObjeto.getMonth() + 1;
   var dia = fechaObjeto.getDate();
 
-  var fechaFormato = ano + '-' + refactorizar(mes) + '-' + refactorizar(dia);
+  var fechaFormato = ano + "-" + refactorizar(mes) + "-" + refactorizar(dia);
 
   function refactorizar(numero) {
-    if (numero < 10)
-      return '0' + numero;
+    if (numero < 10) return "0" + numero;
     return numero;
   }
 
   return (
     <>
       <Button
-        className = "boton-info"
-        rightIcon={<ArrowForwardIcon/>}
+        className="boton-info"
+        rightIcon={<ArrowForwardIcon />}
         ref={btnRef}
         colorScheme="teal"
-        onClick={onOpen}>
+        onClick={onOpen}
+      >
         Más información
       </Button>
       <Drawer
@@ -193,16 +254,17 @@ function InformacionDrawer({ info }) {
         placement="right"
         onClose={onClose}
         finalFocusRef={btnRef}
-        size={'lg'}
+        size={"lg"}
       >
         <DrawerContent className="carta">
           <DrawerHeader className="info-header">Información:</DrawerHeader>
           <DrawerBody className="info">
-            <SimpleGrid columns={{base: 2}}>
+            <SimpleGrid columns={{ base: 2 }}>
               <Image
+                alt="foto-info"
                 className="imagen-personaje"
                 priority
-                style={{ width: '90%', height: 'auto', borderRadius: '4%' }}
+                style={{ width: "90%", height: "auto", borderRadius: "4%" }}
                 src={info.image}
                 width={"100"}
                 height={"100"}
@@ -219,10 +281,15 @@ function InformacionDrawer({ info }) {
                 <></>
               </p>
             </SimpleGrid>
-            <DespliegueEpisodios info={info.episode}/>
+            <DespliegueEpisodios info={info.episode} />
           </DrawerBody>
           <DrawerFooter>
-            <Button className="boton-cerrar" variant="outline" mr={3} onClick={onClose}>
+            <Button
+              className="boton-cerrar"
+              variant="outline"
+              mr={3}
+              onClick={onClose}
+            >
               Cerrar
             </Button>
           </DrawerFooter>
@@ -232,60 +299,68 @@ function InformacionDrawer({ info }) {
   );
 }
 
-function DespliegueEpisodios({info}) {
-  const { isOpen, onToggle } = useDisclosure()
+function DespliegueEpisodios({ info }) {
+  const { isOpen, onToggle } = useDisclosure();
 
   return (
     <>
-      <Button className="boton-episodios" onClick={onToggle} marginTop={2} leftIcon={<ArrowDownIcon />} rightIcon={<ArrowDownIcon />}>Episodios</Button>
+      <Button
+        className="boton-episodios"
+        onClick={onToggle}
+        marginTop={2}
+        leftIcon={<ArrowDownIcon />}
+        rightIcon={<ArrowDownIcon />}
+      >
+        Episodios
+      </Button>
       <Collapse in={isOpen} animateOpacity>
-        <Box
-          p='40px'
-          color='white'
-          mt='4'
-          bg='teal.500'
-          rounded='md'
-        >
+        <Box p="40px" color="white" mt="4" bg="teal.500" rounded="md">
           <UnorderedList>
-            <Text as='b'>Episodio / Al aire / Nombre de episodio</Text>
-            <Text as='div'> <br /> </Text>{/* salto de linea */}
-            {info.map((informacion) =>(
-              <MostrarEpisodios info={informacion}/>
-              ))}
+            <Text as="b">Episodio / Al aire / Nombre de episodio</Text>
+            <Text as="div">
+              {" "}
+              <br />{" "}
+            </Text>
+            {/* salto de linea */}
+            {info.map((informacion, index) => (
+              <MostrarEpisodios key={index} info={informacion} />
+            ))}
           </UnorderedList>
         </Box>
       </Collapse>
     </>
-  )
+  );
 }
 
-function ModalFiltros(){
+function ModalFiltros() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  return(
+  return (
     <>
-      <Button onClick={onOpen} className="boton-avance">Filtros</Button>
+      <Button onClick={onOpen} className="boton-avance">
+        Filtros
+      </Button>
 
-      <Modal isOpen={isOpen} onClose={onClose} size={'lg'}>
+      <Modal isOpen={isOpen} onClose={onClose} size={"lg"}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Filtros</ModalHeader>
-          <ModalCloseButton/>
+          <ModalCloseButton />
           <ModalBody>
             Genero:
-            <CheckboxGroup colorScheme='green'>
-              <Stack spacing={[1, 4]} direction={['column', 'row']}>
-                <Checkbox value='male'>Male</Checkbox>
-                <Checkbox value='female'>Female</Checkbox>
-                <Checkbox value='genderless'>Genderless</Checkbox>
-                <Checkbox value='unknown'>Unknown</Checkbox>
+            <CheckboxGroup colorScheme="green">
+              <Stack spacing={[1, 4]} direction={["column", "row"]}>
+                <Checkbox value="male">Male</Checkbox>
+                <Checkbox value="female">Female</Checkbox>
+                <Checkbox value="genderless">Genderless</Checkbox>
+                <Checkbox value="unknown">Unknown</Checkbox>
               </Stack>
               Status:
             </CheckboxGroup>
-            <CheckboxGroup colorScheme='green'>
-              <Stack spacing={[1, 3]} direction={['column', 'row']}>
-                <Checkbox value='alive'>Alive</Checkbox>
-                <Checkbox value='dead'>Dead</Checkbox>
-                <Checkbox value='unknown'>Unknown</Checkbox>
+            <CheckboxGroup colorScheme="green">
+              <Stack spacing={[1, 3]} direction={["column", "row"]}>
+                <Checkbox value="alive">Alive</Checkbox>
+                <Checkbox value="dead">Dead</Checkbox>
+                <Checkbox value="unknown">Unknown</Checkbox>
               </Stack>
             </CheckboxGroup>
           </ModalBody>
@@ -299,27 +374,26 @@ function ModalFiltros(){
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 }
 
-function MostrarEpisodios({info}){
+function MostrarEpisodios({ info }) {
   const [episodios, setEpisodios] = useState("");
 
   // useEffect se ejecuta cada vez que el valor de episodios cambie
-  useEffect(() => { 
+  useEffect(() => {
     // Que la función sea async permite utilizar await
     const CambioEpisodios = async () => {
       const response = await fetch(info, { method: "GET" });
       const data = await response.json();
       setEpisodios(data);
-    }
+    };
     CambioEpisodios();
   }, [episodios]);
-  
-  return(
+
+  return (
     <ListItem>
       {episodios.episode} ( {episodios.air_date} ); {episodios.name}
     </ListItem>
-  )
+  );
 }
-
